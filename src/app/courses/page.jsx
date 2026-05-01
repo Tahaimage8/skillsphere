1d59165b7b19895eca93e780afab3c7e08dbafe0
tahaimage8 <tahaimage8@gmail.com>
2026-05-01 06:18:43 +0600

    Fix courses page data loading

--- src/app/courses/page.jsx
+++ src/app/courses/page.jsx
@@ -1,29 +1,27 @@
 import CourseCard from "@/components/CourseCard";
 import Search from "@/components/Search";
-// import { div } from "motion/react-client";
-import React from "react";
+import courses from "../../../public/courses.json";
 
-const CoursesPage = async () => {
-  const res = await fetch(
-    "https://skillsphere-ibtesam.vercel.app/courses.json"
-  );
-  const courses = await res.json();
-
-  
-  if(!courses){
-  return <div className="text-white text-center mt-20">Course not found</div>;
+const CoursesPage = () => {
+  if (!courses || courses.length === 0) {
+    return (
+      <div className="mt-20 text-center text-white">
+        Course not found
+      </div>
+    );
   }
+
   return (
     <div className="mx-auto max-w-7xl px-5 py-10">
-      <div className="mb-8 flex flex-col gap-4 lg:flex-col lg:items-center lg:  md:flex-row md:items-center md:justify-between">
-        <h2 className="text-3xl font-bold text-white">All Courses<span className="text-blue-400">.</span></h2>
+      <div className="mb-8 flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
+        <h2 className="text-3xl font-bold text-white">
+          All Courses<span className="text-blue-400">.</span>
+        </h2>
 
-        
         <Search courses={courses} />
-
       </div>
 
-      <div className="grid gap-10 md:grid-cols-3 sm:grid-cols-1">
+      <div className="grid gap-10 sm:grid-cols-1 md:grid-cols-3">
         {courses.map((course) => (
           <CourseCard key={course.id} course={course} />
         ))}
